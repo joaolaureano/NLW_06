b3caa93cc2eae528c6b9c5b31adb9edfa3c6e689
johnnyloreano <laureano.pedrojoao@gmail.com>
2021-06-22 01:13:50 -0300

    Pages DONE

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,12 +1,16 @@
-import { Button } from './components/Button';
+
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-      <Button ></Button>
-      <Button ></Button>
-    </div>
-  )
+    <BrowserRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- letmeask/src/components/Button.tsx
+++ letmeask/src/components/Button.tsx
@@ -1,14 +1,15 @@
+import { ButtonHTMLAttributes } from 'react';
 import { useState } from "react";
 
-export function Button() {
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button(props: ButtonProps) {
 
     const [counter, setCounter] = useState(0)
 
-    function increment() {
-        setCounter(counter + 1);
-
-    }
     return (
-        <button onClick={increment}>Valor do contador [{counter}]</button >
+        <button className="button" {...props} />
     )
 }
